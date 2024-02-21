25c81a5b0e305b5149b362eaa1984328abf8a6f4
Jesus Mendoza Martinez <jsmendoza1312@gmail.com>
2024-02-21 13:51:17 -0500

    second class

--- init-hola/src/assets/App.jsx
+++ init-hola/src/assets/App.jsx
@@ -1,15 +1,19 @@
 import "./Apps.css";
 import { TwFollowCard } from "./TwFollowCard";
 export function App() {
+  const formatUserName = (userName) => `@${userName}`;
+
   return (
     <section className="App">
       <TwFollowCard
+        formatUserName={formatUserName}
         isFollowing
         userName={"jsMendoza"}
         name={"Jesus Mendoza M"}
       />
       <TwFollowCard
-        isFollowing
+        formatUserName={formatUserName}
+        isFollowing={false}
         userName={"jsMendoza13"}
         name={"Jesus Martinez"}
       />
--- init-hola/src/assets/TwFollowCard.jsx
+++ init-hola/src/assets/TwFollowCard.jsx
@@ -1,5 +1,19 @@
-export function TwFollowCard({ userName, name, isFollowing }) {
-  const addAt = (userName) => `@${userName}`;
+import { useState } from "react";
+
+export function TwFollowCard({ formatUserName, userName, name }) {
+  //Se carga Hooks
+  const [isFollowing, setIsFollowing] = useState(false);
+
+  //rederizado condicional
+  const text = isFollowing ? "Siguiendo" : "Seguir";
+  const buttonClassName = isFollowing
+    ? "tw-followCard-btn is-following"
+    : "tw-followCard-btn";
+
+  //Funcion Handle
+  const handleClick = () => {
+    setIsFollowing(!isFollowing);
+  };
 
   return (
     <article className="tw-followCard">
@@ -9,14 +23,22 @@ export function TwFollowCard({ userName, name, isFollowing }) {
           src="https://unavatar.io/soundcloud/gorillaz"
           alt="el avatar de ejemplo"
         />
+
+        {/*asi se pone un comentario en JSX*/}
+
         <div className="tw-followCard-info">
           <strong>{name}</strong>
-          <span className="tw-followCard-infoUsername">{addAt(userName)}</span>
+          <span className="tw-followCard-infoUsername">
+            {formatUserName(userName)}
+          </span>
         </div>
       </header>
 
       <aside>
-        <button className="tw-followCard-btn">Seguir</button>
+        <button className={buttonClassName} onClick={handleClick}>
+          <span className="tw-followcard-text">{text}</span>
+          <span className="tw-followCard-stopFollow">Dejar de seguir</span>
+        </button>
       </aside>
     </article>
   );
